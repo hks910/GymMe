3eca7d3610cf431f1396cf099f6088862e5f67ae
hks910 <hardi.kusuma@binus.ac.id>
2024-06-03 12:18:57 +0700

    add repository user

--- GymMe/Models/MsUser.cs
+++ GymMe/Models/MsUser.cs
@@ -27,6 +27,7 @@ namespace GymMe.Models
         public System.DateTime UserDOB { get; set; }
         public string UserGender { get; set; }
         public string UserRole { get; set; }
+        public string UserPassword { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<MsCart> MsCarts { get; set; }
