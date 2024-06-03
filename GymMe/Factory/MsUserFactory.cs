fa3633f56ab32565d9c8ff6c0be056ffdbb82dd8
hks910 <hardi.kusuma@binus.ac.id>
2024-06-03 08:23:47 +0700

    add database and model

---
+++ GymMe/Factory/MsUserFactory.cs
@@ -0,0 +1,27 @@
+﻿using GymMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace GymMe.Factory
+{
+    public class MsUserFactory
+    {
+        public static MsUser Create(String userName, String userEmail, 
+            DateTime userDOB, String userGender, String userRole)
+        {
+            MsUser user  = new MsUser();
+
+            user.UserName = userName;
+            user.UserEmail = userEmail;
+            user.UserDOB = userDOB;
+            user.UserGender = userGender;
+            user.UserRole = userRole;
+
+            return user;
+           
+        }
+    }
+}
